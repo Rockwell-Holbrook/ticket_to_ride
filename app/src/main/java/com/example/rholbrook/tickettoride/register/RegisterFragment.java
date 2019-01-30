298fe4667b61f084d3f10f57ed8e245e88be66fc
lindonbej <lindonbej@gmail.com>
2019-01-29 18:18:28 -0700

    Added RegisterFragment class and corresponding package, layout file, and string resources

---
+++ app/src/main/java/com/example/rholbrook/tickettoride/register/RegisterFragment.java
@@ -0,0 +1,45 @@
+package com.example.rholbrook.tickettoride.register;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.rholbrook.tickettoride.R;
+
+/**
+ * Created by Blaine Johnson on 1/29/19.
+ */
+
+public class RegisterFragment extends Fragment {
+    private Button mButton;
+    private EditText mUsernameField;
+    private EditText mPasswordField;
+    private EditText mConfPasswordField;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        mButton = v.findViewById(R.id.register_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return v;
+    }
+}
+
