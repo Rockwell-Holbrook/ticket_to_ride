cffaff2d985a997d39f8a0375ae846b0389148c9
tcallred <taylor.allred21@gmail.com>
2019-02-06 15:53:03 -0700

    Fixed problem where gson could not serialize Game because of the ClientProxy field. It's now set to `transient`.

--- server/src/test/java/communication/ServerFacadeTests.java
+++ server/src/test/java/communication/ServerFacadeTests.java
@@ -1,8 +1,14 @@
 package communication;
 
+import com.example.shared.commands.Command;
+import com.example.shared.model.Game;
 import com.example.shared.model.Player;
+import com.google.gson.Gson;
 import game.GameManager;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerFacadeTests {
     @Test
@@ -15,5 +21,21 @@ public class ServerFacadeTests {
         System.out.println(gm.getGameList());
 
 
+    }
+
+    @Test
+    public void gsonTest(){
+        Gson gson = new Gson();
+        String methodName = "updateGameList";
+
+        String[] typeNames = {List.class.getName()};
+
+        List<Game> games = new ArrayList<>();
+        games.add(new Game(new Player("Taylor", true), 5, "my game"));
+        Object[] inputVals = {games};
+
+        String json = gson.toJson(new Command(methodName, typeNames, inputVals));
+        Command cmd = gson.fromJson(json, Command.class);
+        System.out.println(cmd.toString());
     }
 }
