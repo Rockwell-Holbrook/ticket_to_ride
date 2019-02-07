61474af1b3f32e236f60f01549c07cc3256bc36c
Blaine Johnson <lindonbej@gmail.com>
2019-02-06 22:24:04 -0700

    Tweaked register edittexts

--- app/src/main/java/com/example/rholbrook/tickettoride/register/RegisterFragment.java
+++ app/src/main/java/com/example/rholbrook/tickettoride/register/RegisterFragment.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.shared.model.Message;
 import com.example.rholbrook.tickettoride.R;
 
 /**
